b9378deae551daf266eba46a6decdb8f56bf5f95
Sergei1123 <47056438+Sergei1123@users.noreply.github.com>
2022-03-16 19:30:58 +0300

    лр1

---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Начало работы";
+            BackColor = Color.Aqua;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Show();
+        }
+    }
+}
